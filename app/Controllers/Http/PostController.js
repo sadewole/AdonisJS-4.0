d2a81805d204aa1020396b0a3a73e324a0fcde7b
Samuel Adewole <samueladewole15@gmail.com>
2020-01-08 18:25:39 +0100

    init adonis project

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Post = use('App/Models/Post')
+
+class PostController {
+  async index({
+    view
+  }) {
+
+    const posts = await Post.all()
+
+
+    return view.render('post.index', {
+      title: 'Latest Posts',
+      posts: posts.toJSON()
+    })
+  }
+
+  async details({
+    params,
+    view
+  }) {
+    const post = await Post.find(params.id)
+
+    return view.render('post.details', {
+      post: post
+    })
+  }
+}
+
+module.exports = PostController
